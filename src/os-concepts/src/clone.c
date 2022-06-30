92d3b49394c139f458a94905ca265c1df8897f56
ubiquitousbyte <nasdenkov@gmail.com>
2022-06-30 19:22:29 +0200

    format clone example to 80 chars

--- src/os-concepts/src/clone.c
+++ src/os-concepts/src/clone.c
@@ -24,13 +24,15 @@ int main(int argc, char *argv[])
     size_t stack_len = sysconf(_SC_PAGESIZE)*2;
     int protection_flags = PROT_READ | PROT_WRITE;
     int configuration_flags = MAP_ANONYMOUS | MAP_PRIVATE | MAP_STACK;
-    void *stack = mmap(NULL, stack_len, protection_flags, configuration_flags, 0, 0);
+    void *stack = mmap(NULL, stack_len, protection_flags,
+                       configuration_flags, 0, 0);
     if (stack == MAP_FAILED)
         goto err;
 
     /* The parent will share nothing with the child, because no flags are set */
     int shared_resources = 0;
-    pid_t child = clone(child_process, stack + stack_len,  shared_resources | SIGCHLD, NULL);
+    pid_t child = clone(child_process, stack + stack_len,
+                        shared_resources | SIGCHLD, NULL);
     if (child == -1)
         goto err;
 

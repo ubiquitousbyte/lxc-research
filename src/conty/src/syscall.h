5599baa36800574742f9b53684a834aa9ee30ff9
ubiquitousbyte <nasdenkov@gmail.com>
2022-07-29 20:25:28 +0200

    namespace mount syscalls

--- src/conty/src/syscall.h
+++ src/conty/src/syscall.h
@@ -16,18 +16,18 @@ static inline int conty_pidfd_send_signal(int pid_fd, int sig,
     return syscall(SYS_pidfd_send_signal, pid_fd, sig, info, flags);
 }
 
-static inline int fsopen(const char *fsname, unsigned int flags)
+static inline int conty_fsopen(const char *fsname, unsigned int flags)
 {
     return syscall(SYS_fsopen, fsname, flags);
 }
 
-static inline int fspick(int dfd, const char *path, unsigned int flags)
+static inline int conty_fspick(int dfd, const char *path, unsigned int flags)
 {
     return syscall(SYS_fspick, dfd, path, flags);
 }
 
-static inline int fsconfig(int fd, unsigned int cmd,
-                           const char *key, const char *value, int aux)
+static inline int conty_fsconfig(int fd, unsigned int cmd,
+                                 const char *key, const char *value, int aux)
 {
     return syscall(SYS_fsconfig, fd, cmd, key, value, aux);
 }

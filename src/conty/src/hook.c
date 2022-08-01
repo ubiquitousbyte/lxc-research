7a4c9ca6d7c3859f439d904675b4c570237f8df3
ubiquitousbyte <nasdenkov@gmail.com>
2022-08-01 19:14:15 +0200

    update hook to use fork

--- src/conty/src/hook.c
+++ src/conty/src/hook.c
@@ -1,169 +1,75 @@
 #include "hook.h"
-
-#include <errno.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <poll.h>
-#include <sys/wait.h>
-
+#include "resource.h"
 #include "syscall.h"
-#include "clone.h"
 
-#define CONTY_HOOK_TIMEOUT 4000
+#include <sys/wait.h>
+#include <sys/poll.h>
+#include <unistd.h>
 
-#define CONTY_HOOK_EXECVE_ARGS(hook_args, args, len) do { \
-    __typeof(len) ___len = len;                           \
-    args[___len] = NULL;                                  \
-    struct conty_hook_param *item, *tmp;                  \
-    SLIST_FOREACH_SAFE(item, hook_args, next, tmp)        \
-        args[--___len] = item->param;                     \
-} while(0)
-
-int conty_hook_init(struct conty_hook *hook, const char *path)
+static int __conty_hook_exec(const char *prog, const char *argv[], const char *envp[],
+                             const char *buf, size_t buf_len, int timeout)
 {
-    hook->path = path;
-    SLIST_INIT(&hook->args);
-    SLIST_INIT(&hook->envp);
-    hook->argc = 0;
-    hook->envc = 0;
-    hook->timeout_ms = CONTY_HOOK_TIMEOUT;
-    return 0;
-}
+    __CONTY_CLOSE int hook_fd = -EBADF;
+    int ipc[2];
+    int read_end, write_end, sig = SIGTERM, perr, status;
+    struct pollfd poll_fd;
 
-void conty_hook_put_arg(struct conty_hook *hook, struct conty_hook_param *arg)
-{
-    SLIST_INSERT_HEAD(&hook->args, arg, next);
-    ++hook->argc;
-}
+    if (pipe(ipc) != 0)
+        return -1;
+    read_end = ipc[0], write_end = ipc[1];
 
-void conty_hook_put_env(struct conty_hook *hook, struct conty_hook_param *env)
-{
-    SLIST_INSERT_HEAD(&hook->envp, env, next);
-    ++hook->envc;
-}
+    pid_t hook_pid = fork();
+    if (hook_pid < 0)
+        return -1;
 
-int conty_hook_put_timeout(struct conty_hook *hook, int timeout)
-{
-    if (timeout < 0)
-        return -EINVAL;
-    hook->timeout_ms = timeout;
-    return 0;
-}
-
-int conty_hook_exec(struct conty_hook *hook, const char *buf, size_t buf_len)
-{
-
-    int err, pid_fd = -EBADF;
-    int pipes[2];
-    if (pipe(pipes) != 0)
-        return -errno;
-
-    int reader = pipes[0], writer = pipes[1];
-
-    pid_t child = conty_clone3(CLONE_PIDFD, &pid_fd, NULL, 0);
-    if (child < 0) {
-        err = -errno;
-        close(reader);
-        close(writer);
-        return err;
+    if (hook_pid == 0) {
+        close(write_end);
+        dup2(read_end, STDIN_FILENO);
+        close(read_end);
+        execve(prog, (char *const *) argv, (char *const *) envp);
+        _exit(-1);
     }
 
-    if (child == 0) {
-        /*
-         * The child will execute the hook.
-         * Before that, we redirect its standard input stream to the
-         * read end of the pipe where the parent will be dumping data
-         */
-        close(writer);
-        if (dup2(reader, STDIN_FILENO) != STDIN_FILENO)
-            exit(EXIT_FAILURE);
+    close(read_end);
+    ssize_t tx = write(write_end, buf, buf_len);
+    conty_close_fd_function(&write_end);
+    if (tx < 0)
+        return -1;
 
-        /* 1 for the path, 1 for NULL plus everything added by put_arg */
-        const char *argv[hook->argc + 2];
-        argv[0] = hook->path;
-        CONTY_HOOK_EXECVE_ARGS(&hook->args, argv, hook->argc + 1);
+    if ((hook_fd = conty_pidfd_open(hook_pid, 0)) < 0)
+        return -1;
 
-        /* 1 for NULL plus everything added by put_env */
-        const char *envp[hook->envc + 1];
-        CONTY_HOOK_EXECVE_ARGS(&hook->envp, envp, hook->envc);
-
-        execve(hook->path, (char *const *) argv, (char *const *) envp);
-        exit(EXIT_FAILURE);
-    }
-
-    /*
-     * Parent closes the reader as it won't be reading any data and writes
-     * the user-defined buffer into the pipe
-     */
-    close(reader);
-    ssize_t tx = write(writer, buf, buf_len);
-    err = -errno;
-    close(writer);
-    if (tx == -1)
-        goto pidfd_out;
-
-    struct pollfd pfd = {
-            .fd = pid_fd,
-            .events = POLLIN,
-            .revents = 0
-    };
-
-    int timeout = hook->timeout_ms;
-    int sig = SIGTERM;
-    int poll_res, status;
+    poll_fd = (struct pollfd) { .fd = hook_fd, .events = POLLIN, .revents = 0 };
     for (;;) {
-        poll_res = poll(&pfd, 1, timeout);
-        if (poll_res == -1)
-            goto pidfd_err;
+        perr = poll(&poll_fd, 1, timeout);
+        if (perr == -1)
+            return -1;
 
-        if (poll_res == 0) {
-            /**
-             * Child hasn't exited in a timely fashion, so we'll
-             * try to terminate it gracefully first and if that
-             * fails we'll nail it to the ground via a SIGKILL
-             */
-            if (conty_pidfd_send_signal(pid_fd, sig, NULL, 0) != 0)
-                goto pidfd_err;
-
+        if (perr == 0) {
+            if (conty_pidfd_send_signal(hook_fd, sig, NULL, 0) != 0)
+                return -1;
             timeout = 0;
             sig = SIGKILL;
         }
 
-        if (poll_res > 0) {
-            /*
-             * Child has terminated,
-             * so we release the process identifier from the kernel
-             */
-            if (waitpid(child, &status, 0) != child)
-                goto pidfd_err;
+        if (perr > 0) {
+            if (waitpid(hook_pid, &status, 0) != hook_pid)
+                return -1;
 
             if (WIFSIGNALED(status)) {
                 if (WTERMSIG(status) == SIGKILL || WTERMSIG(status) == SIGTERM)
-                    err = -ETIMEDOUT;
-                else
-                    err = -EINTR;
+                    errno = ETIME;
+                return -1;
             }
 
-            if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
-                err = -1;
-
-            goto pidfd_out;
+            return (WIFEXITED(status) && WEXITSTATUS(status) != 0) ? -1 : 0;
         }
     }
+};
 
-pidfd_err:
-    err = -errno;
-pidfd_out:
-    close(pid_fd);
-    return err;
-}
-
-int conty_event_hooks_init(struct conty_event_hooks *hooks)
+int conty_hook_exec(const struct conty_hook *hook, const char *buf,
+                    size_t buf_len, int timeout)
 {
-    SLIST_INIT(&hooks->on_rt_create);
-    SLIST_INIT(&hooks->on_sandbox_created);
-    SLIST_INIT(&hooks->on_sandbox_start);
-    SLIST_INIT(&hooks->on_sandbox_started);
-    SLIST_INIT(&hooks->on_sandbox_stopped);
-    return 0;
+    return __conty_hook_exec(hook->prog, hook->argv, hook->envp,
+                             buf, buf_len, timeout);
 }
--- src/conty/src/hook.h
+++ src/conty/src/hook.h
@@ -11,37 +11,18 @@ extern "C" {
 
 #include <stddef.h>
 #include <unistd.h>
+
 #include "queue.h"
 
-struct conty_hook_param {
-    const char *param;
-    SLIST_ENTRY(conty_hook_param) next;
-};
-
 struct conty_hook {
-    const char                                   *path;
-    SLIST_HEAD(conty_hook_argv, conty_hook_param) args;
-    SLIST_HEAD(conty_hook_env, conty_hook_param)  envp;
-    size_t                                        argc;
-    size_t                                        envc;
-    int                                           timeout_ms;
+    const char             *prog;
+    const char            **argv;
+    const char            **envp;
+    TAILQ_ENTRY(conty_hook) next;
 };
 
-int conty_hook_init(struct conty_hook *hook, const char *path);
-void conty_hook_put_arg(struct conty_hook *hook, struct conty_hook_param *arg);
-void conty_hook_put_env(struct conty_hook *hook, struct conty_hook_param *env);
-int conty_hook_put_timeout(struct conty_hook *hook, int timeout);
-int conty_hook_exec(struct conty_hook *hook, const char *buf, size_t buf_len);
-
-struct conty_event_hooks {
-    SLIST_HEAD(conty_on_rt_create_hooks, conty_hook)       on_rt_create;
-    SLIST_HEAD(conty_on_sandbox_created_hooks, conty_hook) on_sandbox_created;
-    SLIST_HEAD(conty_on_sandbox_start_hooks, conty_hook)   on_sandbox_start;
-    SLIST_HEAD(conty_on_sandbox_started_hooks, conty_hook) on_sandbox_started;
-    SLIST_HEAD(conty_on_sandbox_stopped_hooks, conty_hook) on_sandbox_stopped;
-};
-
-int conty_event_hooks_init(struct conty_event_hooks *hooks);
+int conty_hook_exec(const struct conty_hook *hook, const char *buf,
+                    size_t buf_len, int timeout);
 
 #ifdef __cplusplus
 }; // extern "C"

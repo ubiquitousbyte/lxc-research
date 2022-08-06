a725aca83b982d4d282e257458c5f6aaeea4d9bb
ubiquitousbyte <nasdenkov@gmail.com>
2022-08-06 14:34:12 +0200

    veth network creation and destruction methods added

--- src/conty/src/network.c
+++ src/conty/src/network.c
@@ -1,44 +1,134 @@
 #include "network.h"
 
+#include <net/if.h>
+
 #include <netlink/socket.h>
 #include <netlink/route/link.h>
 #include <netlink/route/link/veth.h>
+#include <netlink/route/link/bridge.h>
 
 #include "resource.h"
 #include "log.h"
 
 CONTY_CREATE_CLEANUP_FUNC(struct rtnl_link *, rtnl_link_put);
-
 CONTY_CREATE_CLEANUP_FUNC(struct nl_sock *, nl_socket_free);
 
-int conty_network_veth_create(const char *hname, const char *sbname, pid_t nspid)
+struct conty_network {
+    /*
+     * Netlink socket used for communicating with the kernel
+     * and configuring network interfaces, ip addresses, routes etc
+     */
+    struct nl_sock *sock;
+    /*
+     * L2 packet forwarder
+     */
+    struct rtnl_link *bridge;
+    /*
+     * Virtual ethernet cable that will be attached to the bridge
+     * in order to enable communication across network namespace boundaries
+     */
+    struct rtnl_link *veth;
+};
+
+struct conty_network *conty_network_create(const char bridge[IFNAMSIZ],
+                                           const char veth0[IFNAMSIZ],
+                                           const char veth1[IFNAMSIZ],
+                                           pid_t nspid)
 {
     int err;
-    CONTY_INVOKE_CLEANER(nl_socket_free) struct nl_sock *sock = NULL;
+    CONTY_INVOKE_CLEANER(conty_network_destroy) struct conty_network *net = NULL;
+    struct rtnl_link *veth_peer;
+
+    net = calloc(1, sizeof(struct conty_network));
+    if (!net)
+        return LOG_FATAL_RET(NULL, "conty_network: out of memory");
 
     /*
      * Open up a netlink socket to the routing subsystem in the kernel
      */
-    sock = nl_socket_alloc();
-    if (!sock)
-        return -LOG_FATAL_RET(-ENOMEM, "conty_network: out of memory");
+    net->sock = nl_socket_alloc();
+    if (!net->sock)
+        return LOG_FATAL_RET(NULL, "conty_network: out of memory");
+
+    err = nl_connect(net->sock, NETLINK_ROUTE);
+    if (err != 0)
+        return LOG_ERROR_RET(NULL, "conty_network: cannot connect to rtnetlink");
 
     /*
-     * Create a virtual ethernet device pair and move the
-     * peer end of the device into the network namespace of the
-     * process defined by nspid
+     * Create a virtual ethernet device pair
      */
-    err = rtnl_link_veth_add(sock, hname, sbname, nspid);
+    net->veth = rtnl_link_veth_alloc();
+    if (!net->veth)
+        return LOG_FATAL_RET(NULL, "conty_network: out of memory");
+
+    veth_peer = rtnl_link_veth_get_peer(net->veth);
+    rtnl_link_set_name(net->veth, veth0);
+    rtnl_link_set_name(veth_peer, veth1);
+    /*
+     * Make sure to move the peer end of the device into the network
+     * namespace of the process identified by nspid
+     */
+    rtnl_link_set_ns_pid(veth_peer, nspid);
+    /*
+     * Issue the creation request to the kernel
+     */
+    err = rtnl_link_add(net->sock, net->veth, NLM_F_CREATE);
     if (err != 0) {
-        return -LOG_ERROR_RET(err, "conty_network: cannot create veth pair %s:%s",
-                              hname, sbname);
+        return LOG_ERROR_RET(NULL, "conty_network: cannot create veth %s<->%s: %s",
+                             veth0, veth1, nl_geterror(err));
     }
 
-    /*
-     * TODO: Now that we've created the veth pair, we need to configure an
-     * TODO: L2 virtual switch a.k.a a bridge that will forward packets
-     * TODO: between the sandbox, the host and the internet/other sandboxes on the system.
-     */
+    rtnl_link_set_ifindex(net->veth, if_nametoindex(veth0));
 
-    return 0;
+    /*
+     * Now we'll create an L2 virtual switch on the host that will
+     * forward packets between our sandbox and other peers attached to the switch
+     */
+    net->bridge = rtnl_link_bridge_alloc();
+    if (!net->bridge)
+        return LOG_FATAL_RET(NULL, "conty_network: out of memory");
+
+    rtnl_link_set_name(net->bridge, bridge);
+    err = rtnl_link_add(net->sock, net->bridge, NLM_F_CREATE);
+    if (err != 0) {
+        return LOG_ERROR_RET(NULL, "conty_network: could not create bridge %s: %s",
+                             bridge, nl_geterror(err));
+    }
+    rtnl_link_set_ifindex(net->bridge, if_nametoindex(bridge));
+
+    /*
+     * Attach host portion of the virtual ethernet device to the bridge
+     */
+    err = rtnl_link_enslave(net->sock, net->bridge, net->veth);
+    if (err != 0) {
+        return LOG_ERROR_RET(NULL, "conty_network: %s could not enslave %s: %s",
+                             bridge, veth0, nl_geterror(err));
+    }
+
+    return CONTY_MOVE_PTR(net);
 }
+
+void conty_network_destroy(struct conty_network *net)
+{
+    if (net) {
+        if (net->sock) {
+            if (net->veth)
+                rtnl_link_delete(net->sock, net->veth);
+            if (net->bridge)
+                rtnl_link_delete(net->sock, net->bridge);
+            nl_socket_free(net->sock);
+            net->sock = NULL;
+        }
+
+        if (net->veth) {
+            rtnl_link_veth_release(net->veth);
+            net->veth = NULL;
+        }
+
+        if (net->bridge) {
+            rtnl_link_put(net->bridge);
+            net->bridge = NULL;
+        }
+    }
+}
+
--- src/conty/src/network.h
+++ src/conty/src/network.h
@@ -5,34 +5,22 @@
 extern "C" {
 #endif
 
-#include <stdint.h>
 #include <unistd.h>
+
 #include <net/if.h>
 
-/*
- * Request object for creating a virtual ethernet device pair
- * One end of the pair will be attached to the sandbox and the other
- * will be attached to the host.
- *
- * Traffic on one end will be forwarded to the other
-*/
-struct conty_network_veth_request {
-    /*
-     * Name of veth device on the host
-     */
-    char hname[IFNAMSIZ];
-    /*
-     * Name of veth device in the sandbox
-     */
-    char sbname[IFNAMSIZ];
-    /*
-     * Process identifier of a process residing in the network namespace
-     * that should own one end of the virtual ethernet device
-     */
-    pid_t nspid;
-};
+#include "resource.h"
 
-int conty_network_veth_create(const char *hname, const char *sbname, pid_t nspid);
+struct conty_network;
+
+
+struct conty_network *conty_network_create(const char bridge[IFNAMSIZ],
+                                           const char veth0[IFNAMSIZ],
+                                           const char veth1[IFNAMSIZ],
+                                           pid_t nspid);
+
+void conty_network_destroy(struct conty_network *net);
+CONTY_CREATE_CLEANUP_FUNC(struct conty_network *, conty_network_destroy);
 
 #ifdef __cplusplus
 }; // extern "C"

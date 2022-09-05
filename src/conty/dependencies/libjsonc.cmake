1349dd35d7a9adcdfa0fbe1f881b5f713668fc31
ubiquitousbyte <nasdenkov@gmail.com>
2022-09-05 19:10:08 +0200

    simplify build procedure and add installation step

--- src/conty/dependencies/libjsonc.cmake
+++
@@ -1,5 +0,0 @@
-include(FetchContent)
-FetchContent_Declare(json-c
-        GIT_REPOSITORY https://github.com/json-c/json-c.git
-        GIT_TAG 2f2ddc1f2dbca56c874e8f9c31b5b963202d80e7)
-FetchContent_MakeAvailable(json-c)
